c2b1de14e2306dd67c80ca5aa5716ee1fb646b28
El Hadi Moussi <50993404+ehmoussi@users.noreply.github.com>
2025-03-14 06:51:22 +0100

    More precise definition of the neighbors

--- src/lib.rs
+++ src/lib.rs
@@ -62,9 +62,9 @@ fn _get_contour_segments(
     level: f64,
     vertex_connect_high: bool,
     mask: &Vec<u8>,
-) -> (Vec<f64>, Vec<Option<usize>>) {
+) -> (Vec<f64>, Vec<u8>) {
     let mut segments = Vec::with_capacity(nb_rows * nb_cols);
-    let mut indices = vec![None; 2 * nb_rows * nb_cols];
+    let mut square_cases: Vec<u8> = Vec::with_capacity((nb_rows - 1) * (nb_cols - 1));
     for r0 in 0..(nb_rows - 1) {
         let r1 = r0 + 1;
         for c0 in 0..(nb_cols - 1) {
@@ -90,19 +90,7 @@ fn _get_contour_segments(
                     | 2 * (ur > level) as u8
                     | 4 * (ll > level) as u8
                     | 8 * (lr > level) as u8);
-            let current_index: usize = r0 * (2 * nb_cols) + c0 * 2;
-            match square_case {
-                1 | 2 | 3 | 4 | 5 | 7 | 8 | 10 | 11 | 12 | 13 | 14 => {
-                    indices[current_index] = Some(segments.len() / 4);
-                }
-                6 | 9 => {
-                    let n = segments.len() / 4;
-                    indices[current_index] = Some(n);
-                    indices[current_index + 1] = Some(n + 1);
-                }
-                0 | 15 => (),
-                other_case => unreachable!("Unexpected case: {}", other_case),
-            }
+            square_cases.push(square_case);
             let square_segments = match square_case {
                 1 => vec![
                     top_x(r0, c0, ul, ur, level),
@@ -225,43 +213,381 @@ fn _get_contour_segments(
                     top_y(r0, c0, ul, ur, level),
                 ],
                 0 | 15 => vec![], // No segments pass through the square
-                other_case => unreachable!("Unexpected case: {}", other_case),
+                other_case => unreachable!("Unexpected square case: {}", other_case),
             };
             segments.extend(square_segments);
         }
     }
-    (segments, indices)
+    (segments, square_cases)
 }
 
-fn assemble_contours(
+fn _print_segment(i: usize, segments: &Vec<f64>) {
+    print!(
+        "[({p1_x}, {p1_y})",
+        p1_x = segments[4 * i],
+        p1_y = segments[4 * i + 1]
+    );
+    print!(
+        ", ({p2_x}, {p2_y})]",
+        p2_x = segments[4 * i + 2],
+        p2_y = segments[4 * i + 3],
+    );
+}
+
+fn _print_segment_by_position(
+    r0: &usize,
+    c0: &usize,
+    nb_cols: &usize,
+    positions: &Vec<usize>,
     segments: &Vec<f64>,
-    indices: &Vec<Option<usize>>,
-    nb_cols: usize,
-    tol: f64,
-) -> Vec<Vec<f64>> {
-    let mut contours = Vec::with_capacity(segments.len() / 4);
-    let mut visited = vec![false; segments.len()];
-    let mut neighbors = Vec::with_capacity(segments.len() / 4);
-    for (index, seg_index) in indices.iter().enumerate() {
-        match seg_index {
-            Some(_) => {
-                let mut neighbors_index = Vec::with_capacity(8);
-                for (i, j) in [(-1, 0), (0, -1), (0, 1), (1, 0)].iter() {
-                    for k in 0..2 {
-                        if let Some(n_index) = index.checked_add_signed(
-                            i * 2 * (nb_cols as isize) + 2 * j + k - (index as isize % 2),
-                        ) {
-                            if let Some(neighbor_index) = indices[n_index] {
-                                neighbors_index.push(neighbor_index);
-                            }
-                        }
+    neighbors: &Vec<usize>,
+) -> () {
+    println!("--------------------------------");
+    let index = r0 * (nb_cols - 1) + c0;
+    if let (Some(&start), Some(&end)) = (positions.get(index), positions.get(index + 1)) {
+        for i in start..end {
+            _print_segment(i, &segments);
+            println!("");
+            for &neighbor_index in neighbors.iter() {
+                if let (Some(&start_nb), Some(&end_nb)) = (
+                    positions.get(neighbor_index),
+                    positions.get(neighbor_index + 1),
+                ) {
+                    for j in start_nb..end_nb {
+                        print!("    --> ");
+                        _print_segment(j, segments);
+                        println!();
                     }
                 }
-                neighbors.push(neighbors_index);
             }
-            None => (),
         }
     }
+}
+
+#[inline(always)]
+fn top_index(r0: &usize, c0: &usize, nb_cols: &usize) -> Option<usize> {
+    r0.checked_add_signed(-1)
+        .and_then(|r| Some(r * (nb_cols - 1)))
+        .and_then(|idx| Some(idx + c0))
+}
+
+#[inline(always)]
+fn left_index(r0: &usize, c0: &usize, nb_cols: &usize) -> Option<usize> {
+    c0.checked_add_signed(-1)
+        .and_then(|c| Some(r0 * (nb_cols - 1) + c))
+}
+
+#[inline(always)]
+fn right_index(r0: &usize, c0: &usize, nb_cols: &usize) -> Option<usize> {
+    Some(r0 * (nb_cols - 1) + (c0 + 1))
+}
+
+#[inline(always)]
+fn bottom_index(r0: &usize, c0: &usize, nb_cols: &usize) -> Option<usize> {
+    Some((r0 + 1) * (nb_cols - 1) + c0)
+}
+
+#[inline(always)]
+fn add_neighbor(index: Option<usize>, neighbors: &mut Vec<usize>, positions: &Vec<usize>) {
+    if let Some(index) = index {
+        if let (Some(&start), Some(&end)) = (positions.get(index), positions.get(index + 1)) {
+            for i in start..end {
+                neighbors.push(i);
+            }
+        }
+    }
+}
+
+fn build_neighbors(
+    square_cases: &Vec<u8>,
+    _segments: &Vec<f64>,
+    nb_cols: usize,
+    vertex_connect_high: bool,
+) -> Vec<Vec<usize>> {
+    let mut segment_positions = vec![0; square_cases.len() + 1];
+    for (i, square_case) in square_cases.iter().enumerate() {
+        let nb_segments = match square_case {
+            1 | 2 | 3 | 4 | 5 | 7 | 8 | 10 | 11 | 12 | 13 | 14 => 1,
+            6 | 9 => 2,
+            0 | 15 => 0,
+            other_case => unreachable!("Unexpected square case: {}", other_case),
+        };
+        segment_positions[i + 1] = segment_positions[i] + nb_segments;
+    }
+    let nb_segments = segment_positions[segment_positions.len() - 1];
+    let mut neighbors = Vec::with_capacity(nb_segments / 4);
+    for (square_index, square_case) in square_cases.iter().enumerate() {
+        let r0 = square_index / (nb_cols - 1);
+        let c0 = square_index % (nb_cols - 1);
+        let mut square_neighbors = Vec::new();
+        match square_case {
+            0 | 15 => (),
+            1 => {
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            2 => {
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            3 => {
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            4 => {
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            5 => {
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            6 => match vertex_connect_high {
+                true => {
+                    add_neighbor(
+                        left_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        top_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    // seg 2
+                    add_neighbor(
+                        right_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        bottom_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                }
+                false => {
+                    add_neighbor(
+                        right_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        top_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    // seg 2
+                    add_neighbor(
+                        left_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        bottom_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                }
+            },
+            7 => {
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            8 => {
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            9 => match vertex_connect_high {
+                true => {
+                    add_neighbor(
+                        top_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        right_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    // seg 2
+                    add_neighbor(
+                        bottom_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        left_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                }
+                false => {
+                    add_neighbor(
+                        top_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        left_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    // seg 2
+                    add_neighbor(
+                        bottom_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                    add_neighbor(
+                        right_index(&r0, &c0, &nb_cols),
+                        &mut square_neighbors,
+                        &segment_positions,
+                    );
+                }
+            },
+            10 => {
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            11 => {
+                add_neighbor(
+                    bottom_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            12 => {
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            13 => {
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    right_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            14 => {
+                add_neighbor(
+                    left_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+                add_neighbor(
+                    top_index(&r0, &c0, &nb_cols),
+                    &mut square_neighbors,
+                    &segment_positions,
+                );
+            }
+            _ => unreachable!("Unexpected square case: {}", square_case),
+        }
+        // _print_segment_by_position(
+        //     current_index,
+        //     &segment_positions,
+        //     &segments,
+        //     &square_neighbors,
+        // );
+        match square_case {
+            0 | 15 => (),
+            1..6 => neighbors.push(square_neighbors),
+            7..9 => neighbors.push(square_neighbors),
+            10..15 => neighbors.push(square_neighbors),
+            6 | 9 => {
+                neighbors.push(square_neighbors.clone());
+                neighbors.push(square_neighbors);
+            }
+            _ => unreachable!("Unexpected square case: {}", square_case),
+        }
+    }
+    neighbors
+}
+
+fn assemble_contours(segments: &Vec<f64>, neighbors: &Vec<Vec<usize>>, tol: f64) -> Vec<Vec<f64>> {
+    let mut contours = Vec::with_capacity(segments.len() / 4);
+    let mut visited = vec![false; segments.len()];
     for first_index in 0..(segments.len() / 4) {
         if visited[first_index] {
             continue;
@@ -318,7 +644,7 @@ fn assemble_contours(
     return contours;
 }
 
-#[inline]
+#[inline(always)]
 fn find_next_segment(
     segments: &Vec<f64>,
     visited: &Vec<bool>,
@@ -346,7 +672,7 @@ fn find_next_segment(
     None
 }
 
-#[inline]
+#[inline(always)]
 fn find_previous_segment(
     segments: &Vec<f64>,
     visited: &Vec<bool>,
@@ -426,11 +752,11 @@ fn marching_squares(
         array_len = array.len(),
         mask_len = mask.len()
     );
-    let (nb_rows, nb_cols) = shape;
-    let (segments, indices) =
+    let (_nb_rows, nb_cols) = shape;
+    let (segments, square_cases) =
         _get_contour_segments(&array, shape.0, shape.1, level, is_fully_connected, &mask);
-    debug_assert_eq!(indices.len(), 2 * nb_rows * nb_cols);
-    assemble_contours(&segments, &indices, nb_cols, tol)
+    let neighbors = build_neighbors(&square_cases, &segments, nb_cols, is_fully_connected);
+    assemble_contours(&segments, &neighbors, tol)
 }
 
 // Marching squares algorithm
